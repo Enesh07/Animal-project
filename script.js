6cea56ba0640bbd67de6d3f1a829940b8d6bd579
Enesh07 <eneshorazova21@gmail.com>
2020-10-07 16:16:16 +0500

    Animal project

---
+++ script.js
@@ -0,0 +1,20 @@
+let myAge = 26;//myage
+let earlyYears = 2*10.5;//earlyyears
+earlyYears *= 10.5;//...
+let laterYears = myAge-2;//...
+laterYears = laterYears*4 ;//..
+let myAgeInDogYears = earlyYears + laterYears;//...
+let myName =prompt("Please Enter Your Name").toLowerCase();
+console.log(
+    "My name is" + myName + ". " + "I am " + myAge + "years old in human years which is " + myAgeInDogYears + "years old in dog years."
+           );//display my name and age in dog years
+  alert(
+    "My name is " + myName + ". " + "I am " + myAge + "years old in human years which is " + myAgeInDogYears + "years old in dog years."
+          );//alert
+          
+
+
+
+
+console.log(earlyYears);
+console.log(laterYears);
